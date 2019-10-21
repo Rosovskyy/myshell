cee63ebe8f3311c8af7fde788f9c84b3f8eef67a
rosovskyy <rosovski9@gmail.com>
2019-10-21 21:08:16 +0300

    pwd cd errno added

---
+++ headers/helpers.h
@@ -0,0 +1,31 @@
+//
+// Created by Serhiy Rosovskyy on 10/19/19.
+//
+
+#ifndef MYSHELL_HELPERS_H
+#define MYSHELL_HELPERS_H
+
+#include <iostream>
+#include <string>
+#include <boost/filesystem.hpp>
+#include <mach-o/dyld.h>
+#include <functional>
+#include <unordered_map>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <unistd.h>
+#include <boost/tokenizer.hpp>
+#include <sys/wait.h>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <errno.h>
+#include <fcntl.h>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+
+void get_current_path(string *str);
+
+#endif //MYSHELL_HELPERS_H
